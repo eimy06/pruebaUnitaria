a4df6b381341371bfc90506458a04ad2e72b03ae
eimy06 <eimylaurafigueroa@gmail.com>
2023-11-19 18:37:33 -0400

    prueba unitaria

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3002;
+const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hola Mundo, Eimy L. Figueroa, 2020-9190');
